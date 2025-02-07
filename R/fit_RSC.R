56144617ff0ee8ef2ad8b58bdc2711d9de16ad48
tcarruth <tomsinbox@gmail.com>
2025-02-07 07:37:05 -0800

    parallel optional

--- R/fit_RSC.R
+++ R/fit_RSC.R
@@ -6,21 +6,25 @@
 #' @param sims Integer or vector of integers - the number of simulations for the operating model (e.g. 96) or the specific vector of simulations for the operating model (e.g. 13:48)
 #' @param max_F Positive real number - the maximum instantaneous mortality rate in any historical time step
 #' @param resample Logical - should parameters be drawn from the variance covariance matrix of the MLE fit (T) or just the MLE parameter values (F)
+#' @param parallel Logical - should model fitting use multiple cores
 #' @param silent Logical - should all RCM messages be repressed (ie not read out onto the console)?
 #' @examples
 #' cond.RSC("In.RSC.7F")
 #' @author T. Carruthers
 #' @seealso \link{RCM}
 #' @export
-cond.RSC = function(RCMinput, sims = 12, max_F=0.5, resample = T, silent=T){
-  setup()
+cond.RSC = function(RCMinput, sims = 12, max_F=0.5, resample = T, parallel = T, silent=T){
+  if(parallel){
+    setup()
+    cores=parallel::detectCores()/2
+  }
   if(length(sims) == 1) simy = 1:sims
   if(length(sims) > 1) simy = sims
   OM = SubCpars(RCMinput$OM, simy)
 
 
   RCMfit = RCM(OM, RCMinput[[3]], s_selectivity = c("B","B"),
-             max_F = max_F, mean_fit = T, condition = "catch", cores = 8,
+             max_F = max_F, mean_fit = T, condition = "catch", cores = cores,
              drop_nonconv=T, drop_highF=T,resample = T, silent=silent)
 
   RCMfit@OM@Name = paste0("Giant Red Sea Cucumber QMA ",RCMinput[[1]])
